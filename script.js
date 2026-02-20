01a0413700656bb073aa9214d52d36c50c0539ab
sinisaglavas <sinisaglavas@ymail.com>
2026-02-20 16:44:59 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let name = 'Sinisa';
+
+let age = 30;
+
+let height = 190.5;
+
+let isProgrammer = true;
+
+console.log(name, age, height, isProgrammer);
+
+let lastName = 'Glavas';
+
+let fullName = name + ' ' + lastName;
+
+console.log(fullName);
+
+// Napraviti 2 varijable, prviBroj = 10, drugiBroj = 20, trecu varijablu zbir = 25
+
+let prviBroj = 10;
+let drugiBroj = 20;
+let zbir = prviBroj + drugiBroj;
+
+console.log(zbir);
+
+// Domaci 1.24
+// Array
+// Napraviti array sa tri stavke: racunar, tastatura, mis
+
+let oprema = ['racunar', 'tastatura', 'mis'];
